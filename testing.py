2fa8baf58e915ea78bfafe26cfb16a67c609d940
unknown <daniel.js.campbell@gmail.com>
2024-01-10 13:19:19 -0400

    test camera setting extraction
    need camera settings for databasing preferably
    
    also edited trial step not to reset camera position after trial

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 import threading
+import subprocess
 
 from system.setup import load_state, load_init_vars, save_state, init_components
 from protocols import rotate_camera_position_onto_endstop, reset_camera_position, \
@@ -10,76 +11,33 @@ STATE = load_state()
 INIT_PARAMS = load_init_vars()
 COMPS = init_components(INIT_PARAMS)
 
-# # Define pin connections
-# CRUSHING_STEP_PIN = 13
-# CRUSHING_DIR_PIN = 27
-# CRUSHING_ENA_PIN = 22  # not currently implemented since using estop instead
-#
-# # Define pin connections
-# CAMERA_STEP_PIN = 12
-# CAMERA_DIR_PIN = 17
-# CAMERA_ENA_PIN = 26  # not currently implemented since using estop instead
-#
-# chan_list = [
-#     CRUSHING_STEP_PIN,
-#     CRUSHING_DIR_PIN,
-#     CRUSHING_ENA_PIN,
-#     CAMERA_STEP_PIN,
-#     CAMERA_DIR_PIN,
-#     CAMERA_ENA_PIN
-# ]
-#
-# # Set up GPIO pins - all gpio config is done here, easier to cleanup (less fancy)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(chan_list, GPIO.OUT)
+
+def get_camera_settings(port):
+    # List all configuration options
+    list_command = ['sudo', 'gphoto2', f'--port {port}', '--list-config']
+    list_result = subprocess.run(list_command, capture_output=True, text=True)
+    config_options = list_result.stdout.splitlines()
+
+    # Retrieve current settings
+    settings = {}
+    for option in config_options:
+        get_command = ['sudo', 'gphoto2', f'--port {port}', '--get-config', option]
+        get_result = subprocess.run(get_command, capture_output=True, text=True)
+        settings[option] = get_result.stdout
+
+    return settings
 
 
-def camera_step():
-
-    reset_camera_position(
-        state=STATE,
-        trigger_event=threading.Event(),
-        verification_cycles=3
-    )
+def main():
 
     cam_ports = gphoto2_get_active_ports()
     for port in cam_ports:
         eosr50_init(port)
 
-    # use last known dir
-    stepper_dir = STATE.get('camera_stepper_last_dir')
-    rotate_camera_position_onto_endstop_with_cameras(
-        state=STATE,
-        stepper_dir=stepper_dir,
-        camera_ports=cam_ports,
-        trigger_event=threading.Event()
-    )
-
-    reset_camera_position(
-        state=STATE,
-        trigger_event=threading.Event(),
-        verification_cycles=3
-    )
-
-    save_state(state=STATE)
-
-    pass
-
-
-def main():
-    camera_step()
-    # print(f"initial last known dir: {STATE.get('camera_stepper_last_dir')}")
-    
-    # reset_camera_position(
-    #     state=STATE,
-    #     trigger_event=threading.Event(),
-    #     verification_cycles=3
-    # )
-
-    # print(f"final last knwon dirL {STATE.get('camera_stepper_last_dir')}")
+    cam_settings = get_camera_settings(cam_ports[0])
+    print(cam_settings)
     
     pass
-
 
 
 if __name__ == '__main__':
---
+++ trial_protocols.py
@@ -0,0 +1,46 @@
+import threading
+
+from system.setup import load_state, load_init_vars, save_state, init_components
+from protocols import reset_camera_position, rotate_camera_position_onto_endstop_with_cameras
+from camera.canon_eosr50 import eosr50_init, gphoto2_get_active_ports
+
+# might need to be careful with states of state and init params
+STATE = load_state()
+INIT_PARAMS = load_init_vars()
+COMPS = init_components(INIT_PARAMS)
+
+
+def trial_step():
+
+    reset_camera_position(
+        state=STATE,
+        trigger_event=threading.Event(),
+        verification_cycles=3
+    )
+
+    cam_ports = gphoto2_get_active_ports()
+    for port in cam_ports:
+        eosr50_init(port)
+
+    # use last known dir
+    stepper_dir = STATE.get('camera_stepper_last_dir')
+    rotate_camera_position_onto_endstop_with_cameras(
+        state=STATE,
+        stepper_dir=stepper_dir,
+        camera_ports=cam_ports,
+        trigger_event=threading.Event()
+    )
+    save_state(state=STATE)
+
+    pass
+
+
+def main():
+    camera_step()
+
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
