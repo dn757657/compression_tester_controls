c66eba7b30baff34f1b73b4b24256a2851f359e5
unknown <daniel.js.campbell@gmail.com>
2024-01-31 13:49:04 -0400

    testing PID controller

--- src/test.py
+++ src/test.py
@@ -1,6 +1,7 @@
 import Encoder
 import time
 
+from simple_pid import PID
 from compression_tester_controls.protocols_dev import load_configs, inst_components
 
 configs = load_configs()
@@ -9,11 +10,19 @@ components = inst_components(component_configs=configs)
 enc = Encoder.Encoder(23, 24)
 big_stepper = components.get('big_stepper')
 
-big_stepper.rotate(freq=500, duty_cyle=85, direction='cw')
+# big_stepper.rotate(freq=500, duty_cyle=85, direction='cw')
+setpoint = 100
+pid = PID(1, 0.1, 0.05, setpoint=setpoint)
+pid.sample_time = 0.01
 
-start = time.time()
+if enc.read() > setpoint:
+    direction = 'cw'
+else:
+    direction = 'ccw'
+
 while True:
-    print(f"{enc.read()}")
-    if time.time() - start >= 1:
-        big_stepper.stop()
-        break
+    enc_pos = enc.read()
+    print(f"encoder position: {enc_pos}")
+
+    freq = pid(enc_pos)
+    big_stepper.rotate(freq=freq, duty_cyle=85, direction=direction)
