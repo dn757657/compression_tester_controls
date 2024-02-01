96effd34c257a872d1562a2b2c53fdcc822e1251
unknown <daniel.js.campbell@gmail.com>
2024-02-01 16:39:07 -0400

    encoder testing via arduino

--- src/test.py
+++ src/test.py
@@ -1,48 +1,68 @@
 import Encoder
 import time
+import serial
 
 from simple_pid import PID
 from compression_tester_controls.protocols_dev import load_configs, inst_components
 
+
+def get_encoder_position(port='/dev/ttyACM0', baudrate=9600):
+    with serial.Serial(port, baudrate, timeout=1) as ser:
+        # Send request
+        ser.write(b'R')  # Request encoder position
+        time.sleep(0.1)  # Short delay to ensure data is processed
+        
+        # Read response
+        if ser.in_waiting > 0:
+            position = ser.readline().decode().strip()
+            return int(position)
+        else:
+            return None
+
+
 configs = load_configs()
 components = inst_components(component_configs=configs)
 
-big_stepper = components.get("big_stepper")
-#big_stepper.rotate(freq=-500, duty_cyle=85)
-#time.sleep(10)
-enc = components.get("e5")
+enc_pos = get_encoder_position()
 
-setpoint = 300
-pid = PID(5, 0, 0, setpoint=setpoint)
+print(f"enc pos: {enc_pos}")
 
-print(f"{components.__str__()}")
+# big_stepper = components.get("big_stepper")
+# #big_stepper.rotate(freq=-500, duty_cyle=85)
+# #time.sleep(10)
+# # enc = components.get("e5")
 
-enc_pos = enc.read()
+# setpoint = 300
+# pid = PID(5, 0, 0, setpoint=setpoint)
 
-pid.output_limits = (-10, 10)
-pid.sample_time = 0.01
+# print(f"{components.__str__()}")
 
-err = 0
+# enc_pos = enc.read()
 
-while True:
-    setpoint = int(input("enter setpoint: "))
-    pid.setpoint = int(setpoint)
-    while True:
-        enc_pos = enc.read()
-        print(f"encoder position: {enc_pos}")
+# pid.output_limits = (-10, 10)
+# pid.sample_time = 0.01
+
+# err = 0
+
+# while True:
+#     setpoint = int(input("enter setpoint: "))
+#     pid.setpoint = int(setpoint)
+#     while True:
+#         enc_pos = enc.read()
+#         print(f"encoder position: {enc_pos}")
         
-        freq = pid(enc_pos)
-        print(f"control command: {freq}")
-        big_stepper.rotate(
-            freq=freq,
-#            direction='ccw',
-            duty_cyle=85
-        )
+#         freq = pid(enc_pos)
+#         print(f"control command: {freq}")
+#         big_stepper.rotate(
+#             freq=freq,
+# #            direction='ccw',
+#             duty_cyle=85
+#         )
 
-        if (setpoint - err) <= enc.read() <= (setpoint + err):
-        #if setpoint == enc.read():
-            big_stepper.stop()
+#         if (setpoint - err) <= enc.read() <= (setpoint + err):
+#         #if setpoint == enc.read():
+#             big_stepper.stop()
             
-            print(f"setpoint reached")
-            print(f"enc pos = {enc_pos}")
-            break
+#             print(f"setpoint reached")
+#             print(f"enc pos = {enc_pos}")
+#             break
