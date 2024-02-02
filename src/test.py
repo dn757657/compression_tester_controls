e87b29bd3130074c4ac2f15f5e50989bb51d7e96
unknown <daniel.js.campbell@gmail.com>
2024-02-02 17:35:18 -0400

    s

--- src/test.py
+++ src/test.py
@@ -12,11 +12,17 @@ def main():
     configs = load_configs()
     components = inst_components(component_configs=configs)
     adc = components.get('force_sensor_adc')
+    a201 = components.get('A201')
     # adc_observer(adc)
 
     while True:
         state_sma = adc.get_state_SMA(n=100, unit='volts')
+        vout = state_sma.get('a1') - state_sma.get('a0')
+        vref = state_sma.get('a3') - state_sma.get('a2')
+        load = a201.get_load(vout=vout, vref=vref)
+
         print(f"{state_sma}")
+        print(f"a201 load: {load}")
 
 # def adc_observer(adc):
 
