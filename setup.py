f33c25f0eeaab484763c298d4a167c7153710c3a
unknown <daniel.js.campbell@gmail.com>
2024-02-07 15:14:04 -0400

    adding json configs in setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name="compression_tester_controls",  # Required
-    version="2.0.18",  # Required
+    version="2.0.19",  # Required
     description="poorly made hardware control library - camera testing",  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
     url="https://github.com/dn757657/compression_tester_controls",  # Optional
     packages=find_packages(where='src'),  # Required
+    package_data={
+        'compression_tester_controls.components': ['configs/*.json'],
+    },
     package_dir={"": "src"},
     python_requires=">=3.8, <4",
 )
